0fe6501f3cdfb809a4311f7558987a9f941b5237
Hamza <hamzamunawar345@gmail.com>
2023-12-26 12:30:46 +0500

    changes in guest-records

--- libs/guest-house-core/src/lib/guest-records/guest-records.component.ts
+++ libs/guest-house-core/src/lib/guest-records/guest-records.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SharedHelpersFieldValidationsModule } from '@general-app/shared/helpers/field-validations';
+import { SharedServicesDataModule } from '@general-app/shared/services/data';
+import { SharedServicesGlobalDataModule } from '@general-app/shared/services/global-data';
+
 @Component({
   selector: 'general-app-guest-records',
   templateUrl: './guest-records.component.html',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuestRecordsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private global: SharedServicesGlobalDataModule,
+    private dataService: SharedServicesDataModule,
+    private valid: SharedHelpersFieldValidationsModule
+  ) {
+
+  }
+
 
   ngOnInit(): void {
+    this.getPartyStatus()
+  }
+
+  partyStatus: any[] = []
+  txtSearch: any = '';
+
+
+
+
+  ///
+  // {
+  //   "partyID": 1,
+  //   "partyFirstName": "Shakeel",
+  //   "partyLastName": "Abbas",
+  //   "partyCNIC": "393402-53533353-9",
+  //   "partyMobile": "03000454535",
+  //   "status": "regular"
+  // }
+  getPartyStatus() {
+    this.dataService.getHttp(`guestms-api/Party/getPartyStatus`, '').subscribe(
+      (response: any[]) => {
+        this.partyStatus = response
+        console.log(response);
+      },
+      (error: any) => {
+        console.log(error)
+      }
+    )
   }
 
 }
